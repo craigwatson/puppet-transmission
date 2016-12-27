10be528215141c2dfcf421a1d34423ce3ed70e99
Craig Watson <craig@cwatson.org>
2016-12-27 22:21:17 +0000

    Using validate_legacy

--- manifests/init.pp
+++ manifests/init.pp
@@ -49,32 +49,60 @@ class transmission (
     fail "Your osfamily (${::osfamily}) is not supported by this module"
   }
 
-  if $blocklist_url != undef { validate_string($blocklist_url) }
-  validate_string($bind_address_ipv4)
-  validate_string($bind_address_ipv6)
+  if $blocklist_url != undef {
+    validate_string($blocklist_url)
+  }
+
+  if $rpc_bind_address {
+    validate_string($rpc_bind_address)
+  }
+
+  if $incomplete_dir {
+    validate_legacy('Stdlib::Compat::Absolute_Path', 'validate_absolute_path', $incomplete_dir)
+  }
+
+  if $log_file {
+    validate_legacy('Stdlib::Compat::Absolute_Path', 'validate_absolute_path', $log_file)
+  }
+
+
+  if $rpc_password {
+    validate_string($rpc_password)
+  }
+
+  if $rpc_whitelist {
+    validate_string($rpc_whitelist)
+  }
+
+  if $speed_limit_down {
+    validate_numeric($speed_limit_down)
+  }
+
+  if $speed_limit_up {
+    validate_numeric($speed_limit_up)
+  }
+
+  validate_legacy('Stdlib::Compat::Absolute_Path', 'validate_absolute_path', $download_dir)
+
   validate_bool($dht_enabled)
-  validate_absolute_path($download_dir)
-  validate_numeric($encryption)
-  if $incomplete_dir { validate_absolute_path($incomplete_dir) }
-  if $log_file { validate_absolute_path($log_file) }
   validate_bool($manage_ppa)
+  validate_bool($pex_enabled)
+  validate_bool($rpc_enable_auth)
+  validate_bool($service_enable)
+  validate_bool($utp_enabled)
+
+  validate_numeric($encryption)
   validate_numeric($message_level)
   validate_numeric($peer_port)
   validate_numeric($peer_port_random_high)
   validate_numeric($peer_port_random_low)
-  validate_bool($pex_enabled)
-  validate_string($rpc_base_url)
-  if $rpc_bind_address { validate_string($rpc_bind_address) }
-  validate_bool($rpc_enable_auth)
-  if $rpc_password { validate_string($rpc_password) }
-  validate_string($rpc_username)
   validate_numeric($rpc_port)
-  if $rpc_whitelist { validate_string($rpc_whitelist) }
+
+  validate_string($bind_address_ipv4)
+  validate_string($bind_address_ipv6)
+  validate_string($rpc_base_url)
+  validate_string($rpc_username)
   validate_string($service_ensure)
-  validate_bool($service_enable)
-  if $speed_limit_down { validate_numeric($speed_limit_down) }
-  if $speed_limit_up { validate_numeric($speed_limit_up) }
-  validate_bool($utp_enabled)
 
   if $rpc_bind_address == undef {
     $rpc_bind = $bind_address_ipv4
